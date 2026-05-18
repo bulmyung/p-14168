f964807df77c69c8284d72a188a680bac98819fe
bulmyung <bulmyun9@gmail.com>
2026-05-19 08:47:59 +0900

    260519-16

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -8,14 +8,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
 
 import java.util.stream.Collectors;
 
@@ -24,45 +25,6 @@ import java.util.stream.Collectors;
 @Validated
 public class PostController {
     private final PostService postService;
-
-    private String getWriteFromHtml() {
-        return getWriteFormHtml("", "", "");
-    }
-
-    private String getWriteFormHtml(String errorMessage, String title, String content) {
-        return """
-                <ul style="color: red;">
-                
-                    %s
-                </ul>
-                
-                <form method="POST" action="doWrite">
-                    <input type="text" name="title" placeholder="제목" value="%s" autofocus>
-                    <br>
-                    <textarea name="content" placeholder="내용">%s</textarea>
-                    <br>
-                    <input type="submit" value="작성">
-                </form>
-                
-                
-                <script>
-                
-                const forms = document.querySelectorAll('form');
-                const lastForm = forms[forms.length - 1];
-                const errorFieldName = lastForm.previousElementSibling?.querySelector('li')?.dataset?.errorFieldName || '';
-                
-                if (errorFieldName.length > 0 ){
-                lastForm[errorFieldName].focus();
-                }
-                </script>
-                """.formatted(errorMessage, title, content);
-    }
-
-    @GetMapping("/posts/write")
-
-    public String showWrite() {
-        return "post/post/write";
-    }
 
 
     @AllArgsConstructor
@@ -76,31 +38,39 @@ public class PostController {
         private String content;
     }
 
+    @GetMapping("/posts/write")
+    public String showWrite(WriteForm form) {
+        return "post/post/write";
+    }
+
     @PostMapping("/posts/doWrite")
-    @ResponseBody
     @Transactional
     public String write(
             @Valid WriteForm form,
-            BindingResult bindingResult
+            BindingResult bindingResult,
+            Model model
     ) {
         if (bindingResult.hasErrors()) {
             String errorFieldName = "title";
             String errorMessage = bindingResult
                     .getFieldErrors()
                     .stream()
-                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-",3))
-                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1],field[0],field[2]))
+                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-", 3))
+                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
+            model.addAttribute("errorMessage", errorMessage);
+            return "post/post/write";
 
-            return getWriteFormHtml(errorMessage, form.getTitle(), form.getContent());
         }
 
         Post post = postService.write(form.getTitle(), form.getContent());
 
-        return "&d번 글이 생성되었습니다.".formatted(post.getId());
-    }
+        model.addAttribute("post", post);
 
+        return "post/post/writeDone";
+
+    }
 }
 
