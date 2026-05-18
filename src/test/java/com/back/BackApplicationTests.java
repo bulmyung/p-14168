5b336aec242ac5578860d89dfaa0cbf1a47244e7
bulmyung <bulmyun9@gmail.com>
2026-05-18 16:13:29 +0900

    name

--- src/test/java/com/back/BackApplicationTests.java
+++ src/test/java/com/back/BackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P141682ApplicationTests {
+class BackApplicationTests {
 
     @Test
     void contextLoads() {
