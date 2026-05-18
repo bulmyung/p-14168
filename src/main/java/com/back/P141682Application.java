f5330e809eb4cc1947bb1a786f8a5485063c2857
bulmyung <bulmyun9@gmail.com>
2026-05-18 16:12:01 +0900

    260518

---
+++ src/main/java/com/back/P141682Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P141682Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P141682Application.class, args);
+    }
+
+}
