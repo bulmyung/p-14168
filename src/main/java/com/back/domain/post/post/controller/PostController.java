e7e2c024cc3cbb8758ec81cb172448305dc6bfd6
bulmyung <bulmyun9@gmail.com>
2026-05-18 17:47:08 +0900

    260518-13

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -2,9 +2,14 @@ package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,27 +58,35 @@ public class PostController {
         return getWriteFromHtml();
     }
 
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "제목은 2자 이상, 20자 이하로 입력가능하니다.")
+        private String title;
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "내용은 2지 이상, 20자 이하로 입력가능합니다.")
+        private String content;
+    }
+
     @PostMapping("/posts/doWrite")
     @ResponseBody
     @Transactional
     public String write(
-            @NotBlank
-
-            @Size(min = 2, max = 20)
-
-            @RequestParam(defaultValue = "")
-
-            String title,
-            @NotBlank
-            @Size(min = 2, max = 100)
-            @RequestParam(defaultValue = "")
-
-            String content
-
+            @Valid WriteForm form,
+            BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
 
+            String errorFieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
 
-        Post post = postService.write(title, content);
+            return getWriteFormHtml(errorFieldName, errorMessage, form.getTitle(), form.getContent());
+        }
+
+        Post post = postService.write(form.getTitle(), form.getContent());
 
         return "&d번 글이 생성되었습니다.".formatted(post.getId());
     }
