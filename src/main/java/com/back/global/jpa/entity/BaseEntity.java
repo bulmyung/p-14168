db6c80cd6845be6d3adbbe3308729087d18a42e9
bulmyung <bulmyun9@gmail.com>
2026-05-19 17:37:13 +0900

    260519-30

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
@@ -21,8 +22,12 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
+
     @CreatedDate
+
     private LocalDateTime createDate;
+
     @LastModifiedDate
+
     private LocalDateTime modifyDate;
 }
